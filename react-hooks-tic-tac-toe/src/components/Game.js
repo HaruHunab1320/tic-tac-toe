28dfc44a43d13d0a0021bc581bf009c67f6d7e8e
Jakob <jakobmsgrant@gmail.com>
2020-10-14 13:44:31 -0700

    time travel implemented

--- react-hooks-tic-tac-toe/src/components/Game.js
+++ react-hooks-tic-tac-toe/src/components/Game.js
@@ -8,31 +8,45 @@ const styles = {
 };
 
 const Game = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
-  const winner = calculateWinner(board);
+  const winner = calculateWinner(history[stepNumber]);
 
   const handleClick = (i) => {
-    const gameState = [...board];
+    const timeInHistory = history.slice(0, stepNumber + 1);
+    const current = timeInHistory[stepNumber];
+    const squares = [...current];
 
     //If user clicks an occupied square, or if game is won, do nothing
-    if (winner || gameState[i]) return;
+    if (winner || squares[i]) return;
 
     //Put X or O in square
-    gameState[i] = xIsNext ? "X" : "O";
-    setBoard(gameState);
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory([...timeInHistory, squares]);
+    setStepNumber(timeInHistory.length);
     setXIsNext(!xIsNext);
   };
 
-  const jumpTo = () => {};
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
 
-  const renderMoves = () => (
-    <button onClick={() => setBoard(Array(9).fill(null))}>Start Game</button>
-  );
+  const renderMoves = () =>
+    history.map((_step, move) => {
+      const destination = move ? `Go to move#${move}` : "Go to start";
+
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{destination}</button>
+        </li>
+      );
+    });
 
   return (
     <>
-      <Board squares={board} onClick={handleClick} />;
+      <Board squares={history[stepNumber]} onClick={handleClick} />;
       <div style={styles}>
         <p>
           {winner
